342ab25e359f49cf6319ffb73715e3d2e515f524
Blake La Pierre <blakelapierre@gmail.com>
2019-02-25 07:24:11 -0500

    dunno what is WIF right now

--- www-ui/src/bitcoinCash.js
+++ www-ui/src/bitcoinCash.js
@@ -6,27 +6,35 @@ import {onFirstRun} from './utils';
 
 import Identifier from './identifier';
 
+
+console.log({bch});
+
+let pk = new bch.PrivateKey();
+
 try {
   const savedKey = localStorage.getItem('bch-private-key');
 
   if (savedKey) {
-
+    pk = new bch.PrivateKey(savedKey);
   }
+  else localStorage.setItem('bch-private-key', pk.toWIF());
 }
 catch (e) {
-  alert('localStorage not supported!!!!!!!! private key may not be saved!!!!!!!!!!!!!!!!!!!!')
+  alert('localStorage not supported!!!!!!!! private key may not be saved!!!!!!!!!!!!!!!!!!!! private key "WIF": ' + pk.toWIF());
 }
 
-console.log({bch});
 
-const pk = new bch.PrivateKey(),
-      pubKey = pk.toPublicKey(),
+const pubKey = pk.toPublicKey(),
       address = pk.toAddress(),
       pubAddress = pubKey.toAddress();
 
 console.log({pk, address, pubKey, pubAddress});
 
 console.log(address.hashBuffer.toString('UTF-8'), pubAddress.toString());
+
+// https://blockchair.com/bitcoin-cash/address/
+// https://blockdozer.com/address/
+
 
 export default {
   data: {
@@ -41,8 +49,8 @@ export default {
     (
       <bitcoin-cash-ui>
         {pubAddress.toString()}
-        <iframe src={`https://blockdozer.com/address/${pubAddress.toString()}`} />
-        <button>send</button>
+        <iframe src={`https://blockchair.com/bitcoin-cash/address/${pubAddress.toString()}`} />
+        <button onClick={}>send</button>
         <button>receive</button>
         <button>mine</button>
       </bitcoin-cash-ui>
